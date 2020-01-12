bf494eec4f13846d2fa2d1666b38944a37e3c1bd
Johnson Vu <johnsonvu96@gmail.com>
2020-01-11 17:43:22 -0800

    Add trigger camera and save to gallery

--- App.js
+++ App.js
@@ -1,19 +1,87 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+import { Camera } from 'expo-camera';
+import * as Permissions from 'expo-permissions';
+import * as MediaLibrary from 'expo-media-library';
+
+export default class App extends React.Component {
+  state = {
+    hasPermission: null,
+    cameraType: Camera.Constants.Type.back,
+  }
+
+  async componentDidMount() {
+    this.getPermissionAsync()
+    MediaLibrary.requestPermissionsAsync();
+  }
+
+  getPermissionAsync = async () => {
+    // Camera Permission
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({ hasPermission: status === 'granted' });
+  }
+
+  handleCameraType=()=>{
+    const { cameraType } = this.state;
+
+    this.setState({cameraType:
+      cameraType === Camera.Constants.Type.back
+      ? Camera.Constants.Type.front
+      : Camera.Constants.Type.back
+    });
+  }
+
+  takePicture = () => {
+    if (this.camera) {
+      this.camera.takePictureAsync({ onPictureSaved: this.onPictureSaved });
+    }
+  }
+
+  onPictureSaved = photo => {
+    // console.log(photo.uri);
+    MediaLibrary.createAssetAsync(photo.uri);
+  } 
+
+  render(){
+    // check permissions
+    const { hasPermission } = this.state;
+    if (hasPermission === null) {
+      return <View />;
+    } else if (hasPermission === false) {
+      return <Text>No access to camera</Text>;
+    }
+
+    // render app
+    return (
+        <View style={{ flex: 1 }}>
+          <Camera style={{ flex: 1 }} type={this.state.cameraType}  ref={ref => {this.camera = ref}}>
+            <View style={{flex:1, flexDirection:"row",justifyContent:"space-between",margin:30}}>
+              <TouchableOpacity
+                style={{
+                  alignSelf: 'flex-end',
+                  alignItems: 'center',
+                  backgroundColor: 'transparent',
+                }}
+                onPress={()=>this.takePicture()}
+                >
+                <Text>Take Photo</Text>
+              </TouchableOpacity>
+
+              {/* Switch camera */}
+              {/* <TouchableOpacity
+                style={{
+                  alignSelf: 'flex-end',
+                  alignItems: 'center',
+                  backgroundColor: 'transparent',
+                }}
+                onPress={()=>this.handleCameraType()}
+                >
+                <Text>Switch Camera</Text>
+              </TouchableOpacity> */}
+            </View>
+          </Camera>
+      </View>
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
