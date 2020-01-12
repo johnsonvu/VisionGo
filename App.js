8d121e9a6183384b1ff259810072f12f7d31e93f
Justin Kwan <justinkkwan@alumni.ubc.ca>
2020-01-12 06:57:13 -0800

    Allow user to send image for a description of objects present spoken back to them (#2)
    * Skeleton, happy path, done
    
    * Sends real image in base64 to google
    
    * Added a little error handling
    
    * Text to speech added

--- App.js
+++ App.js
@@ -4,11 +4,20 @@ import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { Camera } from 'expo-camera';
 import * as Permissions from 'expo-permissions';
 import * as MediaLibrary from 'expo-media-library';
+import * as FileSystem from 'expo-file-system';
+import * as Speech from 'expo-speech';
+
 
 export default class App extends React.Component {
+  baseURL = 'https://vision.googleapis.com';
+  tellUserWait = 'One moment please';
+
   state = {
     hasPermission: null,
     cameraType: Camera.Constants.Type.back,
+    photoTaken: false,
+    googleResults: [],
+    processing: false
   }
 
   async componentDidMount() {
@@ -34,16 +43,85 @@ export default class App extends React.Component {
 
   takePicture = () => {
     if (this.camera) {
-      this.camera.takePictureAsync({ onPictureSaved: this.onPictureSaved });
+      this.setState({ processing: true });
+      Speech.speak(this.tellUserWait, {});
+      this.camera.takePictureAsync({ onPictureSaved: this.onPictureSaved, base64: true });
     }
   }
 
   onPictureSaved = photo => {
-    // console.log(photo.uri);
     MediaLibrary.createAssetAsync(photo.uri);
-  } 
+    console.log(JSON.stringify(photo.base64.substring(0,200)));
+    this.sendToGoogle(photo.base64)
+    .then((response) => {
+      let itemsSeen = response.map(annotation => annotation.name);
+      this.setState({ processing: false, googleResults: itemsSeen });
+      let itemsForSpeech = this.formatToSpeech(itemsSeen);
+      Speech.speak(itemsForSpeech, {});
+      this.setState({ photoTaken: true });
+    });
+  }
+
+  sendToGoogle = async function(base64Image) {
+    try {
+      console.log("THE URL IS: " + this.baseURL);
+      let response = await fetch(this.baseURL + '/v1/images:annotate?key=SECRET_KEY', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            "requests": [
+              {
+                "image": {
+                  "content": base64Image
+                },
+                "features": [
+                  {
+                    "maxResults": 10,
+                    "type": "OBJECT_LOCALIZATION"
+                  }
+                ]
+              }
+            ]
+        })
+      });
+      console.log("waiting...");
+      let responseJson = await response.json();
+      console.log("RESPONSE JSON IS: ");
+      console.log(JSON.stringify(responseJson));
+      if(responseJson && responseJson.responses && responseJson.responses.length>0){
+        return responseJson.responses[0].localizedObjectAnnotations;
+      }
+      else{
+        return [];
+      }
+    } catch (error) {
+      console.error(error);
+      return [];
+    }
+  }
+
+  formatToSpeech = (items) => {
+    console.log("items are: ");
+    console.log(JSON.stringify(items));
+    let spiel = 'In front is: ';
+    items.slice(0, items.length-1).forEach(item => { spiel += ' ' + item + ','});
+    spiel += "and " + items[items.length-1];
+    return spiel;
+  }
 
   render(){
+    if(this.state.photoTaken){
+      return (
+        <View>
+          <Text>We found:</Text>
+          { this.state.googleResults.map((item, index) => { return (<Text key={index} >{item}</Text>);}) }
+        </View>
+      );
+    }
+    
     // check permissions
     const { hasPermission } = this.state;
     if (hasPermission === null) {
@@ -63,7 +141,7 @@ export default class App extends React.Component {
                   alignItems: 'center',
                   backgroundColor: 'transparent',
                 }}
-                onPress={()=>this.takePicture()}
+                onPress={()=>{ if(!this.state.processing) this.takePicture(); }}
                 >
                 <Text>Take Photo</Text>
               </TouchableOpacity>
